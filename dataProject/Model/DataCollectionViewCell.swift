b175f3828da681a46ab7ed33392ef9bc939081a2
Emilysavannahjames <43076722+Emilysavannahjames@users.noreply.github.com>
2018-11-26 12:22:06 -0700

    Initial commit

---
+++ dataProject/Model/DataCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DataCollectionViewCell.swift
+//  dataProject
+//
+//  Created by James, Emily on 11/26/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class DataCollectionViewCell: UICollectionViewCell {
+    
+}
